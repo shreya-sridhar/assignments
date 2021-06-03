a522f2a3ff1add0af8aa88ca3c76a3b923c2969e
shreya-sridhar <shreyamohak@outlook.com>
2021-06-03 01:19:00 -0700

    added methods

---
+++ constants.py
@@ -0,0 +1,4 @@
+SKU_PRICE_MAP = {"STAPOT":10.00,"STARIC":30.00,"STACOF":14.99,"MEDNEW":2.99}
+SKU_TO_ITEM_MAP = {"STAPOT": "Potatoes","STARIC":"Rice","STACOF":"Coffee","MEDNEW":"Newspaper"}
+ITEM_TO_SKU_MAP = {"Potatoes":"STAPOT","Rice":"STARIC","Coffee":"STACOF","Newspaper":"MEDNEW"}
+
---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest 
+from shopping_cart.py import * 
+
+class TestShoppingCartPrice(unittest.TestCase):
+
+    # when each item is only added once or item frequency at most 1
+    def test_item_frequency_one(self):
+        cart = Cart()
+        
+
+        self.assertEqual()
+
+
+
+
